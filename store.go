02dc9c9d491b360371516c41eff452ea88b40e28
klim0v <klim0v-sergey@yandex.ru>
2019-10-18 11:57:23 +0300

    wip

--- store.go
+++ store.go
@@ -40,7 +40,12 @@ func (store *eventsStore) AddEvent(height uint64, event events.Event) {
 }
 
 func (store *eventsStore) LoadEvents(height uint64) events.Events {
-	panic("implement me")
+	store.Lock()
+	if len(store.pubKeys) == 0 {
+		store.loadKeys()
+	}
+	store.Unlock()
+
 }
 
 func (store *eventsStore) FlushEvents() error {
@@ -58,7 +63,7 @@ func (store *eventsStore) FlushEvents() error {
 
 	store.Lock()
 	defer store.Unlock()
-	store.db.Set(getKeyForHeight(store.pending.height), bytes)
+	store.db.Set(uint2byte(store.pending.height), bytes)
 	return nil
 }
 
@@ -68,33 +73,61 @@ func (store *eventsStore) convert(event events.Event) interface{} {
 	case *events.RewardEvent:
 		res = store.convertReward(event.(*events.RewardEvent))
 	case *events.UnbondEvent:
-		// todo: res = store.convertUnbound(event.(*events.UnbondEvent))
+		res = store.convertUnbound(event.(*events.UnbondEvent))
 	case *events.SlashEvent:
-		// todo: res = store.convertSlash(event.(*events.SlashEvent))
-	case *events.CoinLiquidationEvent:
-
+		res = store.convertSlash(event.(*events.SlashEvent))
+	//case *events.CoinLiquidationEvent:
+	default:
+		res = event
 	}
 	return res
 }
 
 func (store *eventsStore) convertReward(rewardEvent *events.RewardEvent) interface{} {
-	return rewardConvert(rewardEvent, store.Key(rewardEvent))
+	return rewardConvert(rewardEvent, store.Key(rewardEvent.ValidatorPubKey[:]))
 }
 
-func (store *eventsStore) Key(rewardEvent *events.RewardEvent) uint8 {
-	for id, v := range store.pubKeys {
-		if string(v[:]) == string(rewardEvent.ValidatorPubKey) {
-			return id
+func (store *eventsStore) convertUnbound(unbondEvent *events.UnbondEvent) interface{} {
+	return convertUnbound(unbondEvent, store.Key(unbondEvent.ValidatorPubKey[:]))
+}
+
+func (store *eventsStore) convertSlash(slashEvent *events.SlashEvent) interface{} {
+	return convertSlash(slashEvent, store.Key(slashEvent.ValidatorPubKey[:]))
+}
+
+const validatorIdsKeyPrefix = "validator"
+const validatorsCountKey = "validators"
+
+func (store *eventsStore) Key(validatorPubKey []byte) uint8 {
+	{
+		strKey := string(validatorPubKey)
+		for id, v := range store.pubKeys {
+			if string(v[:]) == strKey {
+				return id
+			}
 		}
 	}
 	var key [32]byte
-	copy(key[:], rewardEvent.ValidatorPubKey)
-	ID := uint8(len(store.pubKeys))
-	store.pubKeys[ID] = key
-	return ID
+	copy(key[:], validatorPubKey)
+	id := uint8(len(store.pubKeys))
+	store.pubKeys[id] = key
+
+	store.db.Set(append([]byte(validatorIdsKeyPrefix), uint2byte(uint64(id))...), key[:])
+	store.db.Set([]byte(validatorsCountKey), uint2byte(uint64(len(store.pubKeys))))
+	return id
 }
 
-func getKeyForHeight(height uint64) []byte {
+func (store *eventsStore) loadKeys() {
+	count := store.db.Get([]byte(validatorsCountKey))
+	for i := uint64(0); i < binary.BigEndian.Uint64(count); i++ {
+		validatorPubKey := store.db.Get(append([]byte(validatorIdsKeyPrefix), uint2byte(i)...))
+		var key [32]byte
+		copy(key[:], validatorPubKey)
+		store.pubKeys[uint8(i)] = key
+	}
+}
+
+func uint2byte(height uint64) []byte {
 	var h = make([]byte, 8)
 	binary.BigEndian.PutUint64(h, height)
 	return h
--- types.go
+++ types.go
@@ -29,9 +29,27 @@ type slash struct {
 	ID      uint8
 }
 
+func convertSlash(rewardEvent *events.SlashEvent, ID uint8) interface{} {
+	reward := new(slash)
+	copy(reward.Address[:], rewardEvent.Address[:])
+	copy(reward.Coin[:], rewardEvent.Coin[:])
+	reward.Amount = rewardEvent.Amount
+	reward.ID = ID
+	return reward
+}
+
 type unbond struct {
 	Address [20]byte
 	Amount  []byte
 	Coin    [10]byte
 	ID      uint8
+}
+
+func convertUnbound(rewardEvent *events.UnbondEvent, ID uint8) interface{} {
+	reward := new(unbond)
+	copy(reward.Address[:], rewardEvent.Address[:])
+	copy(reward.Coin[:], rewardEvent.Coin[:])
+	reward.Amount = rewardEvent.Amount
+	reward.ID = ID
+	return reward
 }
